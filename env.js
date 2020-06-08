6240779ec4fac0cfdbd8a2e9448aa2e108a6267e
Jim Callanta <mr.jimcallanta@gmail.com>
2020-06-08 16:47:55 +0800

    Setup Git Ignore [2]

--- env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-	emailHost: "smtp.mailtrap.io",
-	emailUsername: "4b220be3a9b29f",
-	emailPassword: "2fb532ce3460f3",
-	emailTo: "",
-	emailFrom: "scrapefriends@ligph.com",
-	emailBody: "<p>Hi!<br/></br>Please see attachment!<br/></br>Thanks!<br/>scrapefriends</p>",
-}
